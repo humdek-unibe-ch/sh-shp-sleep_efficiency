adf78ae95fe664e03a26d54670da0cd39970b015
Stefan Kodzhabashev <stefan.kodzhabashev@psy.unibe.ch>
2020-11-27 17:35:19 +0100

    add sql script for sleep efficiency plugin/trigger

---
+++ db/update_v1.0.0.sql
@@ -0,0 +1,2 @@
+-- add plugin [sleepexpert] sleep efficiency
+INSERT INTO lookups (type_code, lookup_code, lookup_value, lookup_description) values ('plugins', 'calc_sleep_efficiency', '[sleepexpert] Calculate sleep efficiency', 'Calculate sleep efficiency based on the input data');
