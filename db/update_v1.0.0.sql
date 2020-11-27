c5a43dc0d3735c4e888a9df83acc618b5b9681c5
Stefan Kodzhabashev <stefan.kodzhabashev@psy.unibe.ch>
2020-11-27 18:29:27 +0100

    add database version for the plugin

--- db/update_v1.0.0.sql
+++ db/update_v1.0.0.sql
@@ -1,2 +1,6 @@
 -- add plugin [sleepexpert] sleep efficiency
 INSERT INTO lookups (type_code, lookup_code, lookup_value, lookup_description) values ('plugins', 'calc_sleep_efficiency', '[sleepexpert] Calculate sleep efficiency', 'Calculate sleep efficiency based on the input data');
+
+-- add plugin entry in the plugin table
+INSERT INTO plugins (name, version) 
+VALUES ('sleepEfficiency', 'v1.0.0');
